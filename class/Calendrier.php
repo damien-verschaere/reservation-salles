d0007b83cb27f9d7ae55e97843ee5233781aefeb
damien-verschaere <damien.verschaere@laplateforme.io>
2022-01-18 19:58:51 +0100

    objet Calendrier.php

---
+++ class/Calendrier.php
@@ -0,0 +1,58 @@
+<?php
+
+class Calendrier{
+
+    public  $days=['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
+    private $months = ['janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','decembre'];
+    private $_month;
+    private $_year;
+
+
+/**
+ * constructeur calendrier
+ * @param int mois entre 1 et 12
+ * @param int année
+ */
+public function __construct(?int $month=null,?int $year=null){
+    
+    if ($month === null) {
+        $month = intval(date('m'));
+    }
+    if ($year === null ) {
+        $year = intval(date('Y'));
+    }
+
+    if ($month<1 || $month>12){
+        throw new Exception("le mois $month n'est pas valide ");
+    }
+    if ($year<1970) {
+        throw new Exception("l'année est inferieur a 1970");
+    }
+    $this->_month = $month;
+    $this->_year = $year;
+}
+public function getPremierjour(){
+    return new Datetime("{$this->_year}-{$this->_month} -01");
+}
+public function toString():string{
+    return $this->months[$this->_month -1 ] . ' ' . $this->_year;
+}
+public function getSemaines() {
+    $start=$this->getPremierjour();
+    $end = ( clone $start )->modify('+1 month -1day');
+    $semaines= intval($end->format('W')) - intval($start->format('W'));
+    if ($semaines<0) {
+        $semaines = intval($end->format('W'));
+    }
+    return $semaines;
+}
+
+
+
+
+
+
+
+
+
+}
--- views/planning.php
+++ views/planning.php
@@ -1,29 +1,48 @@
 <?php
 
 session_start();
-var_dump($_SESSION['user']['id']);
-require "requires/header2.php";
-require_once "../class/Reservations.php";
-$resa=new Reservation;
+// var_dump($_SESSION['user']['id']);
 
+require_once "../class/Reservations.php";
+require_once "../class/Calendrier.php";
+$resa=new Reservation;
+$calendrier=new Calendrier(1,2022);
+$jour= $calendrier->getPremierjour()->modify('last monday');
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+<?php require "requires/header2.php";  ?>
+
 <main>
-
-<table>
-    <thead>
-        <tr>
-            <th><?php $resa->affichage_resa()?></th>
-           
-        </tr>
-    </thead>
-    <tbody>
-
-    </tbody>
-
+    
+<h1><?= $calendrier->toString()?> </h1>
+<table class="calendar calendar__exception<?= $calendrier->getSemaines();?>semaines">
+    <?php for ($i=0; $i <$calendrier->getSemaines() ; $i++) : ?>
+    <tr>
+        <?php foreach ($calendrier->days as $dateJour=> $day) : ?>
+        <td>
+            <?= $day;?>
+            <?= (clone $jour)->modify("+" .($dateJour + $i * 7) ." days")->format('d')?>
+        </td>
+        <?php endforeach;?> 
+    </tr>
+    
+    <?php endfor;?>
 </table>
 </main>
+
 <?php
 require "requires/footer2.php";
 ?>
+</body>
+</html>
